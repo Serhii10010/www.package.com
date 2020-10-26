31972ae0ec14635a78c024f887bb51789f814d6f
Serhii <serhii10010@gmail.com>
2020-10-26 18:14:01 +0200

    add method returning info of item

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -27,4 +27,17 @@ class CatalogController extends Controller
         ]);
     }
 
+    public function itemsInfo(Request $request, $language) {
+      $ids =  $request->ids;
+      if ($ids != []) {
+        foreach ($ids as $id) {
+          $product = Product::where('id', $id)->get();
+          $products[$id] = $product;
+        }
+        return $products;
+      } else {
+        return null;
+      }
+    }
+
 }
