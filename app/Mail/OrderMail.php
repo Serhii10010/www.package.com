3016218f0eba1dff4c39b088af592be5790a87ee
Serhii <serhii10010@gmail.com>
2020-11-09 21:15:36 +0200

    improved mail

--- app/Mail/OrderMail.php
+++ app/Mail/OrderMail.php
@@ -10,15 +10,16 @@ use Illuminate\Queue\SerializesModels;
 class OrderMail extends Mailable
 {
     use Queueable, SerializesModels;
+    public $orderInfo;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($orderInfo)
     {
-        //
+      $this->orderInfo = $orderInfo;
     }
 
     /**
@@ -28,6 +29,24 @@ class OrderMail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+      return $this->view('emails.order')
+        ->with([
+            'theme' => $this->orderInfo['theme'],
+            'phone' => $this->orderInfo['phone'],
+            'name' => $this->orderInfo['name'],
+            'surname' => $this->orderInfo['surname'],
+            'email' => $this->orderInfo['email'],
+            'comment' => $this->orderInfo['comment'],
+            'delivery' =>
+            [
+              'delivery_way' => $this->orderInfo['delivery']['delivery_way'],
+              'area' => $this->orderInfo['delivery']['area'],
+              'city' => $this->orderInfo['delivery']['city'],
+              'address' => $this->orderInfo['delivery']['address']
+            ],
+            'cart' => $this->orderInfo['cart'],
+            'cart_total' => $this->orderInfo['cart_total'],
+            'count' => $this->orderInfo['count']
+        ]);
     }
 }
--- app/Mail/SimpleMail.php
+++ app/Mail/SimpleMail.php
@@ -28,6 +28,6 @@ class SimpleMail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.simple');
     }
 }
